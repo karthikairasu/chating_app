588ad37302392d91fc8ae90be9ded95161957982
karthikairasu <karthikairasu@users.noreply.github.com>
2019-10-08 20:46:26 +0530

    Update header.php

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
   <title>AnsChat</title>
   <meta charset="utf-8">
-  <meta name="theme-color" content="##75022D">
+  <meta name="theme-color" content="#75022D">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0 shrink-to-fit=no, user-scalable=0">
   <link rel="icon" type="image/png" href="assets/images/logo.png" sizes="194x194">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
